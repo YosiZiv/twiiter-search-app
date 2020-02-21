faba9dd2e88a89b9391555ffaf398437224b92e1
katlany <notan8@gmail.com>
2020-02-21 12:21:21 +0200

    set up app file stracture

---
+++ src/Components/Pages/TwitterSearch.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TwitterSearch.css";
+const TwitterSearchPage = props => {
+  return (
+    <div className='title'>
+      <h1>Twitter Search App</h1>
+    </div>
+  );
+};
+export default TwitterSearchPage;
