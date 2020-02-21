e51b4b252c003cbda7a89c477056e3a477d95b92
katlany <notan8@gmail.com>
2020-02-21 13:32:43 +0200

    edit twitter search page

--- src/Components/Pages/TwitterSearch.js
+++ src/Components/Pages/TwitterSearch.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TwitterSearch.css";
+import TextInput from "../Layout/TextInput";
 const TwitterSearchPage = props => {
+  const [searchTwitterForm, setSearchTwitterForm] = useState({});
+  const {
+    hashtags = "",
+    startDate = null,
+    endDate = null,
+    lang = "en"
+  } = searchTwitterForm;
+  const handleInputChange = e => {
+    const {
+      target: { id, value }
+    } = e;
+    console.log("function work", e.target.id);
+    return setSearchTwitterForm({ ...searchTwitterForm, [id]: value });
+  };
+
   return (
-    <div className='title'>
-      <h1>Twitter Search App</h1>
+    <div className='w-100'>
+      <div className='row d-flex justify'>
+        <div className='mt-5 d-flex '>
+          <h1 className='text-center'>Twitter Search App</h1>
+        </div>
+      </div>
+      <form>
+        <div>
+          <TextInput
+            handleInputChange={handleInputChange}
+            id='hashtags'
+            type='text'
+            defaultValue={hashtags}
+          />
+        </div>
+      </form>
     </div>
   );
 };
