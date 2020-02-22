534402204b8f282ec0f1fcf02d56d545fc6ea1ac
katlany <notan8@gmail.com>
2020-02-22 10:07:36 +0200

    add languag for select component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import TwitterSearhPage from "./Components/Pages/TwitterSearch";
+import languageJson from "./assets/leng.json";
 import "./App.css";
 
 function App() {
+  const [languages, setLanguages] = useState(null);
+
+  useEffect(() => {
+    console.log("IMPORTENT$$$$$$$$$$$$$$$$");
+    const languageObject = JSON.parse(JSON.stringify(languageJson));
+    const languageArray = Object.values(languageObject);
+    setLanguages(languageArray);
+  }, []);
+
   return (
     <div className='container'>
-      <TwitterSearhPage />
+      <TwitterSearhPage languages={languages} />
     </div>
   );
 }
